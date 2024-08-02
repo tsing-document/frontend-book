9f91f80a7a8178b4a28040fbe735db5766866be4
Tsing <ld320321@163.com>
2024-08-03 01:04:11 +0800

    vite 初始化

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,5 +2,6 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
+  { text: "构建工具", icon: "database", link: "/build-tool/vite/introduce.md",  activeMatch: "^/build-tool/$", },
   "/mine/"
 ]);
--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,7 +1,27 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export default sidebar({
-  "/": [
-    "test"
-  ],
-});
+export default sidebar(
+  {
+    "/build-tool/": 
+    [
+      { 
+        text: "构建工具", 
+        prefix: "/build-tool/",
+        icon: "earth-americas",
+        collapsible: true,
+        expanded: false,
+        children: [
+          {
+            text: "Vite 文档",
+            prefix: "/build-tool/vite/",
+            collapsible: true,
+            expanded: false,
+            children: [
+              "introduce"
+            ],
+          }
+        ]
+      }
+    ]
+  }
+);
